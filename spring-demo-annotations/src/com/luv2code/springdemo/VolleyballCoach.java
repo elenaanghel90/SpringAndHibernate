7e552197fa1eaf24d2b8e8f89575987f8628fe2a
Elena Anghel <elenamarianaanghel@gmail.com>
2020-11-05 19:56:42 +0200

    Inject Properties file using annotations: *sport.properties file created *the properties file in the XML config file loaded *the properties values were injected on the VolleyballCoach class with @Value

--- spring-demo-annotations/src/com/luv2code/springdemo/VolleyballCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/VolleyballCoach.java
@@ -1,9 +1,32 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class VolleyballCoach implements Coach{
+public class VolleyballCoach implements Coach {
+
+    //define field injection (Reflection)
+    @Autowired
+    @Qualifier("randomFortuneService")
+    private FortuneService fortuneService;
+
+    //inject the properties values
+    @Value("foo.emailAddress")
+    private String emailAddress;
+
+    @Value("foo.team")
+    private String teamName;
+
+    //set @Qualifier on constructor
+    @Autowired
+    public VolleyballCoach(@Qualifier("randomFortuneService") FortuneService fortuneService) {
+        System.out.println(">> VolleyballCoach: inside constructor using @autowired and @qualifier");
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Run 5 km each day - volleyball!";
